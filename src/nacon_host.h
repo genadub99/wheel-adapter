393ed1f9cc07ba685a70ce4c9165ad1bda55d535
genadub99 <168217492+genadub99@users.noreply.github.com>
2026-08-16 21:42:36 +0300

    Update nacon_host.h

--- src/nacon_host.h
+++ src/nacon_host.h
@@ -1,29 +1,34 @@
 #ifndef NACON_HOST_H
 #define NACON_HOST_H
 
-#include "tusb.h"
+#include "host/usbh_pvt.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-bool nacon_host_init(void);
+void nacon_host_init(void);
 
-bool nacon_host_open(uint8_t rhport,
-                     uint8_t dev_addr,
-                     tusb_desc_interface_t const* itf_desc,
-                     uint16_t max_len);
+bool nacon_host_open(
+    uint8_t rhport,
+    uint8_t dev_addr,
+    tusb_desc_interface_t const* itf_desc,
+    uint16_t max_len
+);
 
-bool nacon_host_set_config(uint8_t dev_addr, uint8_t itf_num);
+void nacon_host_set_config(
+    uint8_t dev_addr,
+    uint8_t itf_num
+);
 
-bool nacon_host_xfer_cb(uint8_t dev_addr,
-                        uint8_t ep_addr,
-                        xfer_result_t result,
-                        uint32_t xferred_bytes);
+bool nacon_host_xfer_cb(
+    uint8_t dev_addr,
+    uint8_t ep_addr,
+    xfer_result_t result,
+    uint32_t xferred_bytes
+);
 
 void nacon_host_close(uint8_t dev_addr);
-
-bool nacon_is_connected(void);
 
 #ifdef __cplusplus
 }
